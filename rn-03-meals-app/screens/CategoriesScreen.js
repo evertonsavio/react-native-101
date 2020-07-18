c84ef5f914d02e92559fd63bbcef81c298ca09d0
Everton Sávio S. Lucas <everluca@outlook.com>
2020-07-18 11:10:51 -0300

    Class Categories Dummy Data

--- rn-03-meals-app/screens/CategoriesScreen.js
+++ rn-03-meals-app/screens/CategoriesScreen.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, FlatList} from 'react-native';
+
+import {CATEGORIES} from '../data/dummy-data';
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = (props) => {
   //console.log(props); ({navigation}) ao inves de (props)
   return (
-    <View style={styles.screen}>
-      <Text>CategoriesScreen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => props.navigation.push('Meals')}
-      />
-    </View>
+    <FlatList
+      /* keyExtractor={(item, index)=> item.id} OLD VERSION JS*/
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
 };
 
@@ -20,6 +29,12 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
     alignItems: 'center',
   },
 });
