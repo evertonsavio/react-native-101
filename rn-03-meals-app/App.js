6938da7cc30e248daa005352c12095cfb13bfbc4
Everton Sávio S. Lucas <everluca@outlook.com>
2020-07-19 10:30:30 -0300

    Tab Navigation with Stack Navigation

--- rn-03-meals-app/App.js
+++ rn-03-meals-app/App.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 import * as Font from 'expo-font';
 import {AppLoading} from 'expo';
-import {Text, Button} from 'react-native';
 import {enableScreens} from 'react-native-screens';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import CategoryMealsScreen from './screens/CategoryMealsScreen';
@@ -14,7 +14,6 @@ import DefaultOptions from './constans/DefaultOptions';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import HeaderButton from './components/HeaderButtom';
 import FavoritesScreen from './screens/FavoritesScreen';
-import CategoriesScreenNavigation from './navigation/CategoriesScreenNavigation';
 
 //////////////////////////////FONTS ASYNC LOAD//////////////////////////////
 const fetchFonts = () => {
@@ -23,10 +22,65 @@ const fetchFonts = () => {
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   });
 };
+
 ////////////////////////////////APP////////////////////////////////////////
 enableScreens();
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const MealsStack = createStackNavigator();
+const DetailsStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+///////////////////////////////////////////////////////////////////////////
+const HomeStackScreen = () => (
+  <HomeStack.Navigator initialRouteName="Home">
+    <HomeStack.Screen
+      name="Home"
+      component={CategoriesScreen}
+      options={{
+        ...DefaultOptions,
+        ...{title: 'Meus Bolinhos e Tortas <3'},
+      }}
+    />
+    <MealsStack.Screen
+      name="Meals"
+      component={CategoryMealsScreen}
+      options={DefaultOptions}
+    />
+    <DetailsStack.Screen
+      name="Details"
+      component={MealDetailScreen}
+      options={{
+        ...DefaultOptions,
+        ...{
+          headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+              <Item
+                title="Favorite"
+                iconName="ios-star"
+                onPress={() => {
+                  console.log('Mark as favorite');
+                }}
+              />
+              <Item
+                title="Favorite2"
+                iconName="ios-star-outline"
+                onPress={() => {
+                  console.log('Mark as favorite');
+                }}
+              />
+            </HeaderButtons>
+          ),
+        },
+      }}
+    />
+  </HomeStack.Navigator>
+);
+
+/* const MealsStackScreen = () => {
+  <MealsStack.Navigator>
+    
+  </MealsStack.Navigator>;
+}; */
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
@@ -44,48 +98,10 @@ export default function App() {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={CategoriesScreenNavigation}
-            options={{
-              ...DefaultOptions,
-              ...{title: 'Meus Bolinhos e Tortas <3'},
-            }}
-          />
-          <Stack.Screen
-            name="Meals"
-            component={CategoryMealsScreen}
-            options={DefaultOptions}
-          />
-          <Stack.Screen
-            name="Details"
-            component={MealDetailScreen}
-            options={{
-              ...DefaultOptions,
-              ...{
-                headerRight: () => (
-                  <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                    <Item
-                      title="Favorite"
-                      iconName="ios-star"
-                      onPress={() => {
-                        console.log('Mark as favorite');
-                      }}
-                    />
-                    <Item
-                      title="Favorite2"
-                      iconName="ios-star-outline"
-                      onPress={() => {
-                        console.log('Mark as favorite');
-                      }}
-                    />
-                  </HeaderButtons>
-                ),
-              },
-            }}
-          />
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeStackScreen} />
+          <Tab.Screen name="Favorites" component={FavoritesScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     </>
   );
