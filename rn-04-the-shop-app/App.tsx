c05ce4fb487823a0c390fd43aa0f09fa976e9b31
Everton Sávio S. Lucas <everluca@outlook.com>
2020-09-13 13:20:47 -0300

    :sparkles: react redux store initial set up

--- rn-04-the-shop-app/App.tsx
+++ rn-04-the-shop-app/App.tsx
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+
+import productsReducer from './store/reducers/products';
+import ProductsOverviewScreen from './screens/shop/ProductsOverviewScreen';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <ProductsOverviewScreen />
+    </Provider>
   );
 }
 
